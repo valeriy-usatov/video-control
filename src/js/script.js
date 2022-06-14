a0985562be4d245a69412448bf3030791ef16dba
Usatov <valeriy-usatov@yandex.ru>
2022-06-15 06:25:32 +0700

    add mouting

--- src/js/script.js
+++ src/js/script.js
@@ -19,5 +19,30 @@ window.addEventListener("DOMContentLoaded",() => {
             arrowTelephone.style.display= "block";
             arrowtelephoneHidden.style.display= "none";
         }
-    });    
+    });   
+    const swiper = new Swiper('.hero__slider', {
+        // Optional parameters
+        
+        loop: true,
+        spaceBetween:30,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+      
+        // If we need pagination
+        pagination: {
+          el: '.hero__pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.hero__arrow-next',
+          prevEl: '.hero__arrow-prev',
+        },
+      
+        // And if we need scrollbar
+       
+    });
+    
 })
