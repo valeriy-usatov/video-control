97b00d6ed58ec77fe619ca8a553d9ef9166719c5
Usatov <valeriy-usatov@yandex.ru>
2022-07-22 04:52:56 +0700

    add question

--- src/js/script.js
+++ src/js/script.js
@@ -125,5 +125,24 @@ window.addEventListener("DOMContentLoaded",() => {
           }, 
          
     });
-
+    if (document.querySelectorAll(".question__item")) {
+        let accardion=document.querySelector(".question__accordion"),
+            tab=document.querySelectorAll(".question__item"),
+            answer= document.querySelectorAll(".question__answer");
+        
+        accardion.addEventListener("click", (e) => {   
+            const target= e.target.closest(".question__item"); 
+            if(target) {
+                tab.forEach((item,i) => {
+                    if (item===target) {
+                        answer[i].classList.add("active");
+                        tab[i].classList.add('active-accardion');
+                    } else {
+                        answer[i].classList.remove("active");
+                        tab[i].classList.remove('active-accardion')
+                    }
+                })
+            }
+        })
+    }    
 })
